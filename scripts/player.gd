edb8057d5c67f202b243e527ae2f365f90f27d50
DRespess1 <daniel.respess1@gmail.com>
2025-09-07 21:52:20 -0400

    New tread-based movement code

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends MeshInstance3D
 
-@export var acceleration = 1
-@export var deceleration = 3
-@export var angular_acceleration = 0.6* (PI / 180)
-@export var angular_deceleration = 0.9 * (PI / 180)
+@export var acceleration = 100
+@export var deceleration = 30
+@export var angular_acceleration = 3
+@export var angular_deceleration = 6.9 * (PI / 180)
 
 const EPSILON: float = 0.001
 var velocity 
@@ -15,38 +15,54 @@ func _ready():
 	
 func _process(delta):
 	
-	var is_accelerating = false 
-	var is_angular_accelerating = false
+	var tread_speed_left =  0
+	var tread_speed_right = 0
 	
-	if Input.is_action_pressed("forward"):
-		velocity += acceleration * transform.basis.z * delta
-		is_accelerating = true
-	if Input.is_action_pressed("left"):
-		angular_velocity += angular_acceleration * delta
-		is_angular_accelerating = true
-	if Input.is_action_pressed("right"):
-		angular_velocity -= angular_acceleration * delta
-		is_angular_accelerating = true
-	if Input.is_action_pressed("back"): 
-		velocity -= acceleration * transform.basis.z * delta
-	if Input.is_action_just_pressed("fire"):
-		pass
+	
+	tread_speed_left = Input.get_axis("left_axis_back", "left_axis_forward")
+	tread_speed_right = Input.get_axis("right_axis_back", "right_axis_forward")
+		
+	#take lowest speed 
+	velocity = min(abs(tread_speed_left), abs(tread_speed_right)) * acceleration * transform.basis.z * delta
+	
+	angular_velocity = (-tread_speed_left + tread_speed_right) * angular_acceleration * delta
 	
 	rotate(Vector3.UP, angular_velocity)
 	transform.origin = transform.origin + velocity * delta 
 	
 	
-	var norm = velocity.length()
-	if norm != 0 and !is_accelerating:
-		velocity /= norm 
-		norm += -deceleration * delta
-		velocity *= norm 
-	
-	if !is_angular_accelerating:
-		if angular_velocity < 0: 
-			angular_velocity += delta * angular_deceleration
-		else: 
-			angular_velocity -= delta * angular_deceleration
+	#var is_accelerating = false 
+	#var is_angular_accelerating = false
+	#
+	#if Input.is_action_pressed("forward"):
+		#velocity += acceleration * transform.basis.z * delta
+		#is_accelerating = true
+	#if Input.is_action_pressed("left"):
+		#angular_velocity = angular_acceleration * delta
+		#is_angular_accelerating = true
+	#if Input.is_action_pressed("right"):
+		#angular_velocity = -angular_acceleration * delta
+		#is_angular_accelerating = true
+	#if Input.is_action_pressed("back"): 
+		#velocity -= acceleration * transform.basis.z * delta
+		#is_accelerating = true
+	#if Input.is_action_just_pressed("fire"):
+		#pass
+	#
+	#rotate(Vector3.UP, angular_velocity)
+	#transform.origin = transform.origin + velocity * delta 
+	#
+	#var norm = velocity.length()
+	#if norm != 0 and !is_accelerating:
+		#velocity /= norm 
+		#norm += -deceleration * delta
+		#velocity *= norm 
+	#
+	#if !is_angular_accelerating:
+		#if angular_velocity < 0: 
+			#angular_velocity += delta * angular_deceleration
+		#else: 
+			#angular_velocity -= delta * angular_deceleration
 		
 	#angular_velocity =  lerp(angular_velocity, 0.0, angular_velocity * .1)
 	#print(angular_velocity)
