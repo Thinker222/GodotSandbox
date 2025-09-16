5bcc2707c25d4b8c0ad8d320365060f5908bd3d8
Thinker222 <es0197@yahoo.com>
2025-09-15 21:28:32 -0400

    Add bullets and rotation of turret. (Note: It is broken)

---
+++ scripts/bullet.gd
@@ -0,0 +1,28 @@
+class_name Bullet 
+extends RigidBody3D
+
+
+@export var bullet_timeout = 10.0 
+@export var bullet_force = 1
+var bullet_direction 
+
+var cumulative_time 
+
+func _ready():
+	gravity_scale = 0.0
+	pass
+
+func init():
+	cumulative_time = 0
+	bullet_direction  = -global_transform.basis.z
+	apply_impulse(bullet_direction * bullet_force, Vector3.ZERO)
+	print("Creating Bullet and applying impulse")
+	print(transform.origin)
+	
+
+func _process(delta):
+	cumulative_time += delta  
+	if cumulative_time >= bullet_timeout:
+		print("Destroying bullet")
+		queue_free()
+	
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,24 @@
 extends MeshInstance3D
 
+#@onready var skeleton: Skeleton3D = $player_tank/Armature/Skeleton3D
 @export var acceleration = 100
 @export var deceleration = 30
 @export var angular_acceleration = 3
 @export var angular_deceleration = 6.9 * (PI / 180)
+@export var bullet_prefab = preload("res://prefabs/bullet.tscn")
 
 const EPSILON: float = 0.001
 var velocity 
 var angular_velocity
+var cumulative_delta 
+var last_bullet_fired
 
 func _ready():
 	velocity = Vector3.ZERO 
 	angular_velocity = 0.0
-	
+	cumulative_delta = 0.0
+	last_bullet_fired = 0.0	
+		
 func _process(delta):
 	
 	# Get analog inputs 
@@ -35,42 +41,20 @@ func _process(delta):
 	rotate(Vector3.UP, angular_velocity)
 	transform.origin = transform.origin + velocity * delta 
 	
+	var skeleton = $player_tank/Armature/Skeleton3D
 	
-	#var is_accelerating = false 
-	#var is_angular_accelerating = false
-	#
-	#if Input.is_action_pressed("forward"):
-		#velocity += acceleration * transform.basis.z * delta
-		#is_accelerating = true
-	#if Input.is_action_pressed("left"):
-		#angular_velocity = angular_acceleration * delta
-		#is_angular_accelerating = true
-	#if Input.is_action_pressed("right"):
-		#angular_velocity = -angular_acceleration * delta
-		#is_angular_accelerating = true
-	#if Input.is_action_pressed("back"): 
-		#velocity -= acceleration * transform.basis.z * delta
-		#is_accelerating = true
-	#if Input.is_action_just_pressed("fire"):
-		#pass
-	#
-	#rotate(Vector3.UP, angular_velocity)
-	#transform.origin = transform.origin + velocity * delta 
-	#
-	#var norm = velocity.length()
-	#if norm != 0 and !is_accelerating:
-		#velocity /= norm 
-		#norm += -deceleration * delta
-		#velocity *= norm 
-	#
-	#if !is_angular_accelerating:
-		#if angular_velocity < 0: 
-			#angular_velocity += delta * angular_deceleration
-		#else: 
-			#angular_velocity -= delta * angular_deceleration
-		
-	#angular_velocity =  lerp(angular_velocity, 0.0, angular_velocity * .1)
-	#print(angular_velocity)
+	var turret_transform : Transform3D = skeleton.get_bone_global_pose(1)
+	cumulative_delta += delta
+	var rotation_amount : Quaternion = Quaternion(Vector3.UP, delta)
+	var quaternion = turret_transform.basis.get_rotation_quaternion() * rotation_amount
+	turret_transform.basis = Basis(quaternion)
+	skeleton.set_bone_global_pose(1, turret_transform.basis)
 	
-	
+	if cumulative_delta - last_bullet_fired > 1.0:
+		var new_bullet = bullet_prefab.instantiate()
+		new_bullet.global_transform = Transform3D(Basis(quaternion), transform.origin)
+		get_tree().current_scene.add_child(new_bullet)
+		new_bullet.init()
+		last_bullet_fired = cumulative_delta
+		print("trigger")
 	
