e7a92f5898e9024f4917ccb6866bbc954a4a4cce
Thinker222 <es0197@yahoo.com>
2025-09-15 23:19:24 -0400

    Fix bullets to now fire from tank even when base tank has been translated and rotated

--- scripts/player.gd
+++ scripts/player.gd
@@ -49,11 +49,11 @@ func _process(delta):
 	var quaternion = turret_transform.basis.get_rotation_quaternion() * rotation_amount
 	turret_transform.basis = Basis(quaternion)
 	skeleton.set_bone_global_pose(1, turret_transform)
-	
+	var bullet_basis = Basis(transform.basis.get_rotation_quaternion() * turret_transform.basis.get_rotation_quaternion())
 	
 	if cumulative_delta - last_bullet_fired > 1.0:
 		var new_bullet = bullet_prefab.instantiate()
-		new_bullet.global_transform = Transform3D(turret_transform.basis, turret_transform.origin + turret_transform.basis.z * 1.3)
+		new_bullet.global_transform = Transform3D(bullet_basis, transform.origin + turret_transform.origin + bullet_basis.z * 1.3)
 		get_tree().current_scene.add_child(new_bullet)
 		new_bullet.init()
 		last_bullet_fired = cumulative_delta
