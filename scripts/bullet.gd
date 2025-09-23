e0c46ae2d824d786ead5fe074e62704c359ba065
DRespess1 <daniel.respess1@gmail.com>
2025-09-22 20:55:09 -0400

    added tank aiming and manual shooting

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -3,7 +3,7 @@ extends RigidBody3D
 
 
 @export var bullet_timeout = 10.0 
-@export var bullet_force = 5
+
 var bullet_direction 
 
 var cumulative_time 
@@ -12,17 +12,17 @@ func _ready():
 	gravity_scale = 0.0
 	pass
 
-func init():
+func init(bullet_force):
 	cumulative_time = 0
 	bullet_direction  = global_transform.basis.z
 	apply_impulse(bullet_direction * bullet_force, Vector3.ZERO)
-	print("Creating Bullet and applying impulse")
-	print(transform.origin)
+	#print("Creating Bullet and applying impulse")
+	#print(transform.origin)
 	
 
 func _process(delta):
 	cumulative_time += delta  
 	if cumulative_time >= bullet_timeout:
-		print("Destroying bullet")
+		#print("Destroying bullet")
 		queue_free()
 	
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,9 @@ extends MeshInstance3D
 @export var angular_acceleration = 3
 @export var angular_deceleration = 6.9 * (PI / 180)
 @export var bullet_prefab = preload("res://prefabs/bullet.tscn")
+@export var turret_rotation_speed = 5
+
+@export var bullet_force = 15
 
 const EPSILON: float = 0.001
 var velocity 
@@ -13,17 +16,29 @@ var angular_velocity
 var cumulative_delta 
 var last_bullet_fired
 
+var tank_skeleton
+
 func _ready():
 	velocity = Vector3.ZERO 
 	angular_velocity = 0.0
 	cumulative_delta = 0.0
 	last_bullet_fired = 0.0	
-		
-func _process(delta):
 	
+	tank_skeleton = $player_tank/Armature/Skeleton3D
+		
+func _process(delta):	
+	tank_locomotion(delta)	
+	turret_rotation(delta)
+	
+	if Input.is_action_just_pressed("fire"):
+		fire()
+		
+func tank_locomotion(delta):
 	# Get analog inputs 
 	var tread_speed_left = Input.get_axis("left_axis_back", "left_axis_forward")
 	var tread_speed_right = Input.get_axis("right_axis_back", "right_axis_forward")
+	#print(tread_speed_left)
+	#print(tread_speed_right)
 	
 	var velocity = 0.0
 
@@ -39,23 +54,31 @@ func _process(delta):
 	angular_velocity = (-tread_speed_left + tread_speed_right) * angular_acceleration * delta
 	
 	rotate(Vector3.UP, angular_velocity)
-	transform.origin = transform.origin + velocity * delta 
+	transform.origin = transform.origin + velocity * delta 	
+
+func turret_rotation(delta):	
+	var turret_transform : Transform3D = tank_skeleton.get_bone_global_pose(1)
 	
-	var skeleton = $player_tank/Armature/Skeleton3D
-	
-	var turret_transform : Transform3D = skeleton.get_bone_global_pose(1)
-	cumulative_delta += delta
-	var rotation_amount : Quaternion = Quaternion(Vector3.UP, delta)
+	var dir : float
+	if Input.is_action_pressed("left_shoulder"):
+		dir = -1
+	if Input.is_action_pressed("right_shoulder"):
+		dir = 1
+	if Input.is_action_pressed("left_shoulder") and Input.is_action_pressed("right_shoulder"):
+		dir = 0
+		
+	var rotation_amount : Quaternion = Quaternion(Vector3.UP, delta * dir * turret_rotation_speed)
 	var quaternion = turret_transform.basis.get_rotation_quaternion() * rotation_amount
 	turret_transform.basis = Basis(quaternion)
-	skeleton.set_bone_global_pose(1, turret_transform)
+	tank_skeleton.set_bone_global_pose(1, turret_transform)
+	
+
+func fire():	
+	var turret_transform : Transform3D = tank_skeleton.get_bone_global_pose(1)
 	var bullet_basis = Basis(transform.basis.get_rotation_quaternion() * turret_transform.basis.get_rotation_quaternion())
 	
-	if cumulative_delta - last_bullet_fired > 1.0:
-		var new_bullet = bullet_prefab.instantiate()
-		new_bullet.global_transform = Transform3D(bullet_basis, transform.origin + turret_transform.origin + bullet_basis.z * 1.3)
-		get_tree().current_scene.add_child(new_bullet)
-		new_bullet.init()
-		last_bullet_fired = cumulative_delta
-		print("trigger")
-	
+	var new_bullet = bullet_prefab.instantiate()
+	new_bullet.global_transform = Transform3D(bullet_basis, transform.origin + turret_transform.origin + bullet_basis.z * 1.3)
+	get_tree().current_scene.add_child(new_bullet)
+	new_bullet.init(bullet_force)
+	last_bullet_fired = cumulative_delta
